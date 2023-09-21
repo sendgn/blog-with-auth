568da076f4cea1433a093fac8fa67136101fae2f
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-21 17:34:55 +0300

    Add login action

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -21,22 +21,38 @@
     >
 
     <button>Log in</button>
+    <div v-if="error">{{ error }}</div>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 
 export default {
   setup() {
     const email = ref('');
     const password = ref('');
+    const error = ref(null);
 
-    const handleSubmit = () => {
-      console.log(email.value, password.value);
-    };
+    const store = useStore();
+    const router = useRouter();
 
-    return { handleSubmit, email, password };
+    const handleSubmit = async () => {
+      try {
+        await store.dispatch('login', {
+          email: email.value,
+          password: password.value
+        });
+        // redirect user to the home page
+        router.push('/');
+      } catch (err) {
+        error.value = err.mesage;
+      }
+    }
+
+    return { handleSubmit, email, password, error };
   }
 };
 </script>
