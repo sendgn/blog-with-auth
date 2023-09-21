a6fa7d157246e75e49b35ae113ec437f912ac168
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-21 15:27:51 +0300

    Add an action

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,33 @@ const store = createStore({
   state: {
     user: null
   },
+  // Code inside a mutation must be synchronous
+  // https://vuex.vuejs.org/guide/mutations.html#mutations-must-be-synchronous
   mutations: {
     setUser(state, payload) {
       state.user = payload;
       console.log('user state changed:', state.user);
+    }
+  },
+  // Actions are just functions,
+  // and those functions can contain any buisness logic
+  // and asynchronous code like authentication requests,
+  // and then once that request is complete it can commit a mutation
+  // to actually change the state
+  // ---------------------------------
+  // actions can commit many mutations
+  actions: {
+    // Parameters:
+    // 1) context - context object that has a commit() method
+    // which we use to commit a mutation once we've done the asynchronous code
+    // 2) payload - any data we're sending to the action
+    signup(context, { email, password }) {
+      console.log('signup action');
+
+      // async code
+      setTimeout(() => {
+        context.commit('setUser', { email, password });
+      }, 2000);
     }
   }
 });
